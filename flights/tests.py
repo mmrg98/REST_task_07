097de758c86e10863afe36af5ab7ad7e88c2cc9d
mmrg98 <mariam.elgaria98@gmail.com>
2020-09-20 00:32:33 +0300

    done

--- flights/tests.py
+++ flights/tests.py
@@ -9,6 +9,7 @@ from .models import Flight, Booking
 
 
 class FlightListTest(APITestCase):
+	
 	def setUp(self):
 		self.flight1 = {'destination': 'Wakanda', 'time': '10:00', 'price': 230, 'miles': 4000}
 		self.flight2 = {'destination': 'La la land', 'time': '00:00', 'price': 1010, 'miles': 1010}
@@ -128,12 +129,12 @@ class BookingDetails(APITestCase):
 
 	def test_url_forbidden(self):
 		response = self.client.post(reverse('login'), {"username" : "laila2", "password": "1234567890-="})
-		self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])		
+		self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])
 		response = self.client.get(reverse('booking-details', args=[1]))
 		self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 		response = self.client.post(reverse('login'), {"username" : "laila", "password": "1234567890-="})
-		self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])		
+		self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])
 		response = self.client.get(reverse('booking-details', args=[3]))
 		self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
@@ -346,7 +347,7 @@ class BookingCreate(APITestCase):
 	def test_url_unauthorized(self):
 		response = self.client.post(reverse('book-flight', args=[1]), self.data)
 		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-		
+
 
 	def test_creation_works(self):
 		response = self.client.post(reverse('login'), self.user_data)
@@ -401,10 +402,3 @@ class Register(APITestCase):
 
 		response = self.client.post(reverse('login'), {"username" : data["username"], "password":data["password"]})
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-
-
-
-
-
-
