90bc4bf4026ef8d42e337cc447055919190f52cf
mmrg98 <mariam.elgaria98@gmail.com>
2020-09-20 00:31:11 +0300

    done

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from datetime import datetime
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Flight, Booking
 from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterSerializer, AdminUpdateBookingSerializer
@@ -10,6 +11,8 @@ from .permissions import IsBookingOwner, IsChangable
 class FlightsList(ListAPIView):
 	queryset = Flight.objects.all()
 	serializer_class = FlightSerializer
+	filter_backends = [SearchFilter,OrderingFilter,]
+	search_fields = ['destination']
 
 
 class BookingsList(ListAPIView):
@@ -58,5 +61,3 @@ class BookFlight(CreateAPIView):
 
 class Register(CreateAPIView):
 	serializer_class = RegisterSerializer
-
-	
